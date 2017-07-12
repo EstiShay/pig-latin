02c7426a0d998e0eb6ecc65a422093388ea08623
Collin Person, Esti Shay and Maria Thomas <esti@>
2017-07-12 15:01:43 -0700

    modify function to find first vowel in words beginning with consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
-
 var vowels = "aeiouAEIOU";
 var userInput = "";
 
 var translator = function(userInput){
   var firstChar = userInput[0];
-  if (vowels.search(firstChar)>=0) {
+  if (vowels.search(firstChar)> -1) {
     return userInput + "way";
-  } else if (vowels.search(firstChar)===-1){
-    return userInput;
+  } else if (vowels.search(firstChar)=== -1){
+    var userArray = Array.from(userInput);
+    var vowelPosition = '';
+    var vowelFound = false
+    for (var index=0; vowelFound != true; index += 1) {
+      if (vowels.search(userArray[index]) > -1) {
+        vowelPosition = [index];
+        vowelFound = true;
+        alert(vowelPosition);
+      }
+    }
   }
 };
 
